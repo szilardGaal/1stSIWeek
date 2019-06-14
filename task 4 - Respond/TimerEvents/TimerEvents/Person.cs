50f8791c5f21015212ed7b1139cb7a52ef329568
Gaál Szilárd <gaalszilard23@gmail.com>
2019-06-14 14:13:35 +0200

    task 4 complete

---
+++ task 4 - Respond/TimerEvents/TimerEvents/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerEvents
+{
+    class Person
+    {
+        protected String name { get; set; }
+        protected DateTime birthDate { get; }
+        protected Gender gender { get; }
+
+        public Person(String name, DateTime birthDate, String gender)
+        {
+            this.name = name;
+            this.birthDate = birthDate;
+            Gender genderEnum;
+            Enum.TryParse<Gender>(gender, out genderEnum);
+            this.gender = genderEnum;
+        }
+
+        
+        public virtual string toString()
+        {
+            string genderString = gender.ToString();
+            return birthDate + " " + genderString + " " + name;
+        }
+    }
+
+    enum Gender
+    {
+        MALE,
+        FEMALE
+    }
+}
